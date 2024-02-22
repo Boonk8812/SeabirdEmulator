4308bbf38ec1d759bc50344abc8205d8b4649354
Boonk3312 <theboonk7@hotmail.com>
2024-02-22 15:42:47 -0800

    Update java.h

--- src/java.h
+++ src/java.h
@@ -62,3 +62,4 @@ private:
 };
 
 java$Files -> point(send, file):: > "$applets = .j_applet, # treat the file as a java applet (legacy support)\nfiles\": {\".class\": \"JavaCompiledClassFile\", \".java\": \"JavaSourceFile\"}"
+applet applet
