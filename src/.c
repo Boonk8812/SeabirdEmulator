da870ae904b4ba532257aa16e4ca8515c7a96896
Boonk3312 <theboonk7@hotmail.com>
2024-03-02 11:48:15 -0800

    Add files via upload

---
+++ src/.c
@@ -0,0 +1,61 @@
+struct Allocator : RAM_Allocator {
+
+	RAM: {
+
+		 int address = 0x0000, 0xFFFF;
+		 int preloader_memory = 0x000000, 0xFFFFFF;
+	}
+	void allocate() {
+
+
+                 void .preloader_memory(0x4C);
+		 typedef static struct {
+
+			 int Bit(0xA704);
+			 int Num_memory(address);
+			 static struct Memory_contents {
+                   
+				 int contents = {
+
+				          typedef short GDT;
+					  typedef short IDT;
+					  void _idt() {
+
+                                               typedef unsigned int IDT_0;
+					       typedef IDT_0 uintptr_t;
+					       typedef IDT_0 size_t;
+				          }
+				 }
+			 }
+		 } 
+	}
+
+	for i(;;) {
+
+	        if (nullptr) {
+                    break;
+		} else {
+                    continue;
+		}
+        }
+        void allocate() {
+
+            	for i(;;) {
+
+                    int _allocate_byte_() = 0x03F5;
+	            int _allocate_preload_byte() = 0x0040;
+		    int _allocate_limit() = 0xFFFFFFFFFFF;
+		    int _allocate_preload_limit() = 0xFFFFFFFFFFFFFFFFF;
+
+		    _allocate_byte_()
+		    _allocate_preload_byte()
+		    _allocate_limit()
+		    _allocate_preload_limit()
+		    
+		    int _allocate_memory() = address, preloader_memory;
+		    int _memory() = address, preloader_memory;
+
+	    	}
+	}
+	allocate(memory);
+}
